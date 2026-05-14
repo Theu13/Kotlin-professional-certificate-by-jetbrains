73b8f36dd03f3147a7c33b50ff85fdfc612f1950
Theu13 <matheuslimabatista30@gmail.com>
2026-05-14 18:23:03 -0300

    Enum Classes

---
+++ src/main/kotlin/POO/Enum.kt
@@ -0,0 +1,20 @@
+package POO
+
+fun main() {
+
+    val direction: Direction = Direction.WEST
+
+    //println(direction.name)
+    //println(direction.ordinal)
+
+    //val parseDirection = Direction.valueOf("kotlin")
+    //println(direction.name)
+
+    //val apiValue = Direction.values()[2]
+    //println(apiValue.name)
+
+    val color = HighlightColor.BLUE
+    println(color.value)
+
+
+}
---
+++ src/main/kotlin/POO/HighlightColor.kt
@@ -0,0 +1,10 @@
+package POO
+
+enum class HighlightColor(val value: Int) {
+    YELLOW(0xebba34),
+    BLUE(0x3495eb),
+    PURPLE(0x34f4f4)
+
+
+
+}
