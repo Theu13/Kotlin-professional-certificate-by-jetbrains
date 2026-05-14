634dde9aa9dadfb709af3f28226239398bf691b3
Theu13 <matheuslimabatista30@gmail.com>
2026-05-14 17:58:29 -0300

    POO: Um pouco de herança e polimorfismo.

---
+++ src/main/kotlin/POO/MainPOO.kt
@@ -0,0 +1,9 @@
+package POO
+
+fun main() {
+
+    val person: Person = Student(32, "Jhon", "Jota")
+    person.printName()
+
+
+}
---
+++ src/main/kotlin/POO/Student.kt
@@ -0,0 +1,16 @@
+package POO
+
+class Student(
+    age: Int,
+    firstName: String,
+    lastName: String
+): Person(age, firstName, lastName) {
+
+    override fun printName() {
+        println("Student: $firstName $lastName")
+    }
+
+
+
+}
+
